f6c4a1d233b570cde92643194c3c76f6b8f39d78
Gonzalo Ron <gonzaloronvera@gmail.com>
2022-10-10 13:20:54 +1100

    Update deploy.py
    Changing to Goerli

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -17,7 +17,7 @@ def get_account():
     if network.show_active() == "development":
         return accounts[0]
     else:
-        return accounts.load("my-rinkeby-account")
+        return accounts.load("my-goerli-account")
 
 
 def main():
